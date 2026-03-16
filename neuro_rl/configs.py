aaf8b619069e0d6f60e1669198a969b014b13fa8
Marié del Valle Reyes <97261080+marie0501@users.noreply.github.com>
2026-03-16 17:24:45 -0600

    add configs file

---
+++ neuro_rl/configs.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#TASK CONSTANTS
+DT = 0.1 # Time step
+T_TIMEOUT = 15 # Time out
+GOAL_POS = np.array([0.5, 0.5]) # Goal position
+WALL = None
+# WALL = [[0.8, 0.0], [0.8, 0.8]]
+GOAL_RADIUS = 0.1
+REWARD = 1 # Reward
+REWARD_DURATION = 1 # Reward duration
+
+#LEARNING CONSTANTS
+TAU = 5 # Discount time horizon
+TAU_E = 5 # Eligibility trace time horizon
+ETA = 0.01 # Learning rate 
+N_EPISODES = 3 # Number of episodes
+L2 = 0.000 # L2 regularization
